7538a2d41de3304e0c1f4906a5ec937f9b18d206
honguyenducanh <79430306+honguyenducanh@users.noreply.github.com>
2021-02-22 13:34:14 +0700

    AppJava

--- src/com/laptrinh/git/App.java
+++ src/com/laptrinh/git/App.java
@@ -3,6 +3,7 @@ package com.laptrinh.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("App");
     }
 
 }
--- src/com/laptrinh/git/Test.java
+++
@@ -1,8 +0,0 @@
-package com.laptrinh.git;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-
-}
