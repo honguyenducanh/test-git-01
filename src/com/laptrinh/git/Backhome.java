c465993c111cd081f145edab40f0368e0f4eeb33
honguyenducanh <79430306+honguyenducanh@users.noreply.github.com>
2021-02-22 13:39:12 +0700

    Backhomego

--- src/com/laptrinh/git/Backhome.java
+++ src/com/laptrinh/git/Backhome.java
@@ -3,5 +3,6 @@ package com.laptrinh.git;
 public class Backhome {
     public static void main(String[] args) {
         System.out.println("gobackhome");
+        System.out.println("gobackhome`123");
     }
 }
